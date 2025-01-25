f8fc9c5499bff3315ca59fe91c7e707428aaa602
Mike <57708391+Downedzubat@users.noreply.github.com>
2025-01-25 19:42:52 +0000

    First there was no code, but then I got bored and said
    Let there be code

--- Game/Source/GGJ2025/Private/Paper2DCharacter.cpp
+++ Game/Source/GGJ2025/Private/Paper2DCharacter.cpp
@@ -2,13 +2,22 @@
 
 
 #include "Paper2DCharacter.h"
-
+#include "AbilitySystemComponent.h"
+#include "CharacterAttributeSet.h"
 // Sets default values
 APaper2DCharacter::APaper2DCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComp");
+	Attributes = CreateDefaultSubobject<UCharacterAttributeSet>("Attributes");
+
+}
+
+UAbilitySystemComponent* APaper2DCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +25,32 @@ void APaper2DCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this, this);
+
+		if (DefaultAttributeEffect)
+		{
+			FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+			EffectContext.AddSourceObject(this);
+			FGameplayEffectSpecHandle SpecHandle = AbilitySystemComponent->MakeOutgoingSpec(DefaultAttributeEffect, 1, EffectContext);
+
+			if (SpecHandle.IsValid())
+			{
+				FActiveGameplayEffectHandle GEHandle = AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+			}
+		}
+
+		//give da abilities
+		if (HasAuthority())
+		{
+			for (TSubclassOf<UGameplayAbility>& StartupAbility: DefaultAbilities)
+			{
+				AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(StartupAbility.GetDefaultObject(), 1, 0));
+			}
+		}
+	}
 }
 
 // Called every frame
--- Game/Source/GGJ2025/Public/Paper2DCharacter.h
+++ Game/Source/GGJ2025/Public/Paper2DCharacter.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "GameplayTagContainer.h"
+#include <GameplayEffectTypes.h>
+#include "AbilitySystemInterface.h"
 #include "Paper2DCharacter.generated.h"
 
 UCLASS()
@@ -11,9 +14,17 @@ class GGJ2025_API APaper2DCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Abilities", meta = (AllowPrivateAccess = "true"))
+	class UAbilitySystemComponent* AbilitySystemComponent;
+
+	UPROPERTY()
+	class UCharacterAttributeSet* Attributes;
+
 public:
 	// Sets default values for this character's properties
 	APaper2DCharacter();
+
+	UAbilitySystemComponent* GetAbilitySystemComponent() const;
 
 protected:
 	// Called when the game starts or when spawned
@@ -25,5 +36,11 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "RPG")
+	TSubclassOf <class UGameplayEffect> DefaultAttributeEffect;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "RPG")
+	TArray<TSubclassOf <class UGameplayAbility>> DefaultAbilities;
 
 };
